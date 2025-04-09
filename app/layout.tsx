0c3adfe6c58fcc8a782e3d77ea7882c65862f90f
jawad-elhajjami <jawadelhajjamiofficiel@gmail.com>
2025-04-09 17:30:40 +0100

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "To-do App",
+  description: "Web application to track your tasks",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      <h1>To do app</h1>
+    </div>
+  );
+}

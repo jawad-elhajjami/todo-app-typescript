23db6c4f23ab9d27d3bd51ed2c7f1714d8b8ba43
jawad-elhajjami <jawadelhajjamiofficiel@gmail.com>
2025-04-10 17:47:11 +0100

    framer motion animations

--- components/DoneTasks.tsx
+++ components/DoneTasks.tsx
@@ -1,4 +1,6 @@
 import TaskItem from "./TaskItem";
+import { AnimatePresence, motion } from "framer-motion";
+
 type DoneTasksProps = {
     tasks: Task[],
     setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
@@ -7,10 +9,23 @@ const DoneTasks:React.FC<DoneTasksProps> = ({tasks, setTasks}) => {
     return ( 
         <div className="mt-4">
             <h3 className="text-center mt-8 font-semibold text-lg">Done tasks</h3>
-            <div className="mt-2 flex flex-col items-center">
-                {
-                    tasks.map((task) => task.completed ? <TaskItem todo={task} setTasks={setTasks} key={task.id} /> : '')
-                }
+            <div className="mt-2 flex flex-col items-center max-w-lg m-auto">
+                <AnimatePresence>
+                    {
+                        tasks.map((task) => task.completed ? 
+                            <motion.div
+                                key={task.id}
+                                initial={{ opacity: 0, y: -10 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: 10, scale: 0.95 }}
+                                transition={{ duration: 0.3 }}
+                                className="w-full"
+                            >
+                                <TaskItem todo={task} setTasks={setTasks} key={task.id} /> 
+                            </motion.div>
+                        : '')
+                    }
+                </AnimatePresence>
             </div>
         </div>
     );
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,6 +1,8 @@
 'use client'
 import TaskItem from "./TaskItem";
 import { Notebook } from "lucide-react"
+import { AnimatePresence, motion } from "framer-motion";
+
 
 type TodoListProps = {
     tasks: Task[],
@@ -11,15 +13,28 @@ const TodoList:React.FC<TodoListProps> = ({tasks, setTasks}) => {
     return ( 
        <div>
             <h3 className="text-lg font-semibold text-center">To-do List</h3>
-            <div className="flex flex-col items-center mt-8">
+            <div className="flex flex-col items-center mt-8 max-w-lg m-auto">
+                <AnimatePresence>
                 {tasks.filter(task => !task.completed).length === 0 ? 
                     <div className="flex flex-col items-center ">
                         <Notebook className="w-20 h-20 mb-4 text-gray-200 dark:text-zinc-800" />
                         <p className="opacity-50">You're all done for today !</p>
-                    </div> : 
+                    </div>
+                     : 
                 tasks.map(todo => (
-                    !todo.completed ? <TaskItem todo={todo} setTasks={setTasks} key={todo.id}  /> : ''
+                    !todo.completed ? 
+                        <motion.div
+                            key={todo.id}
+                            initial={{ opacity: 0, y: -10 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: 10, scale: 0.95 }}
+                            transition={{ duration: 0.3 }}
+                            className="w-full"
+                        >
+                            <TaskItem todo={todo} setTasks={setTasks} key={todo.id}  /> 
+                        </motion.div> : ''
                 ))}
+                </AnimatePresence>
             </div>
        </div> 
     );
